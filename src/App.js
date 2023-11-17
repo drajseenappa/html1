f6d41d4fefa195153408a68a3e6974f03d67936c
drajseenappa <seenappadheeraj@gmail.com>
2023-11-17 10:09:14 +0530

    first time

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Body from './components/Body'
+import Header from './components/Header'
+import Footer from './components/Footer'
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
